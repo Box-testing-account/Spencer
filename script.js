df693123e0ab25d81d7c825d13588b91d124885d
Box-testing-account <118285563+Box-testing-account@users.noreply.github.com>
2022-11-15 12:44:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+addEventListener("click", function() {
+    var el = document.documentElement, 
+    rfs = el.webkitRequestFullScreen;
+    rfs.call(el);
+});
+
+
+
 var submit = document.getElementById('submit');
 var download = document.getElementById('download');
 var url = document.getElementById('url');
