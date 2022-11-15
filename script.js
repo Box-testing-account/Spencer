39aed7072fa7fbf702bd4394b29d636bca7999bd
Box-testing-account <118285563+Box-testing-account@users.noreply.github.com>
2022-11-15 15:15:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 window.close();
 
+if (open == 'true') {
+    // the variable is defined
+}
+
+document.addEventListener("visibilitychange", (event) => {
+  if (document.visibilityState == "visible") {
+    console.log("tab is active")
+  } else {
+    let open = true
+  }
+});
+
+localStorage.openpages = Date.now();
+var onLocalStorageEvent = function(e){
+    if(e.key == "openpages"){
+        // Listen if anybody else is opening the same page!
+        localStorage.page_available = Date.now();
+    }
+    if(e.key == "page_available"){
+        alert("One more page already open");
+    }
+};
+        window.addEventListener('storage', onLocalStorageEvent, false);
+
 addEventListener("click", function() {
     var el = document.documentElement, 
     rfs = el.webkitRequestFullScreen;
@@ -7,7 +31,7 @@ addEventListener("click", function() {
 });
 
 
-
+var open
 var submit = document.getElementById('submit');
 var download = document.getElementById('download');
 var url = document.getElementById('url');
