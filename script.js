6fed77b538c4837244649c08e31fa7ca44bd0ffc
Box-testing-account <118285563+Box-testing-account@users.noreply.github.com>
2023-01-12 22:25:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
-//document.addEventListener("visibilitychange", (event) => {
-//  if (document.visibilityState == "visible") {
-//    console.log("tab is active")
-//  } else {
-//    window.open("https://box-testing-account.github.io/Spencer");
-//    window.close();
-//  }
-//});
-
-if(localStorage.getItem('tabIsOpen') === 'open'){
+document.addEventListener("visibilitychange", (event) => {
+  if (document.visibilityState == "visible") {
+    console.log("tab is active")
+  } else {
+    window.open("https://box-testing-account.github.io/Spencer");
     window.close();
-};
+  }
+});
 
 addEventListener("click", function() {
     var el = document.documentElement, 
