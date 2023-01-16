bb040b8efedb91d3474033abd39f87a9aad0f911
Box-testing-account <118285563+Box-testing-account@users.noreply.github.com>
2023-01-16 10:49:10 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,53 @@
-document.addEventListener("visibilitychange", e => 
-  document.hidden && window.close()
-);
-resizeTo(screen.availWidth, screen.availHeight);
-addEventListener("resize", e => 
-    resizeTo(screen.availWidth, screen.availHeight)
-);
-setInterval(() => 
-  screen.availWidth + screen.availHeight && resizeTo(screen.availWidth, screen.availHeight)
-, 100);
-setInterval(() => window.focus(), 500);
-setInterval(() => 
-  localStorage.getItem("activated") === "true"
-    ? window.opener || (window.open("https://box-testing-account.github.io/Spencer-Trap/"), setTimeout(window.close, 100))
-    : window.close()
-, 100);
-addEventListener("click", () => 
-    document.documentElement.webkitRequestFullScreen()
-);
+document.addEventListener("visibilitychange", (event) => {
+  if (document.visibilityState == "visible") {
+  } else {
+    window.close();
+  }
+});
+
+resizeTo(window.screen.availWidth, window.screen.availHeight);
+addEventListener("resize", (event) => {
+    resizeTo(window.screen.availWidth, window.screen.availHeight);
+});
+setInterval(() => {
+    if (window.screen.availWidth + window.screen.availHeight == 0) {
+    } else {
+        resizeTo(window.screen.availWidth, window.screen.availHeight);
+    };
+}, 100);
+
+setInterval(function(){
+    window.focus();
+}, 500);
+
+
+setInterval(function() {
+  if (localStorage.getItem("activated") === "true") {
+    if (window.opener) {
+    } else {
+      window.open("https://box-testing-account.github.io/Spencer-Trap/");
+      setTimeout(window.close(), 100);
+    }
+    console.log("Activated is true");
+  } else {
+    // Code to run if "activated" is false
+    console.log("Activated is false");
+    window.close();
+  }
+}, 100);
+
+addEventListener("click", function() {
+    var el = document.documentElement, 
+    rfs = el.webkitRequestFullScreen;
+    rfs.call(el);
+});
+
+var open
 var submit = document.getElementById('submit');
 var download = document.getElementById('download');
 var url = document.getElementById('url');
-submit.onclick = () => 
-  document.getElementById('instructions') && document.getElementById('instructions').remove();
+submit.onclick = () => {
+	if(document.getElementById('instructions')) {
+		document.getElementById('instructions').remove();
+	}
+};
