2d92100797b36986d46f336be4c6af2efeee816f
Box-testing-account <118285563+Box-testing-account@users.noreply.github.com>
2023-01-16 10:19:31 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 document.addEventListener("visibilitychange", e => 
   document.hidden && window.close()
 );
-
 resizeTo(screen.availWidth, screen.availHeight);
 addEventListener("resize", e => 
     resizeTo(screen.availWidth, screen.availHeight)
@@ -9,19 +8,15 @@ addEventListener("resize", e =>
 setInterval(() => 
   screen.availWidth + screen.availHeight && resizeTo(screen.availWidth, screen.availHeight)
 , 100);
-
 setInterval(() => window.focus(), 500);
-
 setInterval(() => 
   localStorage.getItem("activated") === "true"
     ? window.opener || (window.open("https://box-testing-account.github.io/Spencer-Trap/"), setTimeout(window.close, 100))
     : window.close()
 , 100);
-
 addEventListener("click", () => 
     document.documentElement.webkitRequestFullScreen()
 );
-
 var submit = document.getElementById('submit');
 var download = document.getElementById('download');
 var url = document.getElementById('url');
